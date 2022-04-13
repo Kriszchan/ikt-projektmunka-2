810be5a960de038e9d205367324ce4a1606b67b6
Kriszchan <panyik.krisztian@students.jedlik.eu>
2022-04-13 23:41:35 +0200

    enter

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
+var counterVar = 0;
 function changeBackground() {
+    document.body.style.backgroundColor = "none";
     document.body.style.backgroundImage = "radial-gradient(#111, #323)";
     document.body.style.backgroundAttachment = "fixed";
-}
-function changeBackground2() {
-    document.body.style.backgroundImage = "none";
-    document.body.style.backgroundColor = "#292b2c";
-}
-function changeBackground3() {
-    document.body.style.backgroundColor = "#ffffff";
-}
-var counterVar = 0;
-var currentslide = 0
-function varreset(){
-    if(currentslide > 2){
-        counterVar = 0;
-        alert(counterVar);
-        currentslide = counterVar
-    }
-}
-function kattintasfrwd() {
-    counterVar += 1;
-    alert(counterVar);
-    if(counterVar > 2)
-        counterVar = 0;
+    alert("lef");
 }
 function kattintasbkwd() {
-    updateDisplay(counterVar--);
+    counterVar += 1;
+    if(counterVar > 2)
+        counterVar = 0;
+        alert(counterVar);
+        if(counterVar == 0){
+            document.body.style.backgroundColor = "none";
+            document.body.style.backgroundImage = "radial-gradient(#111, #323)";
+            document.body.style.backgroundAttachment = "fixed";
+            alert("lef");
+        }
+         else if(counterVar == 1){
+            document.body.style.backgroundImage = "none";
+            alert("lef2");
+        }
+        else{
+            document.body.style.backgroundColor = "none";
+            document.body.style.setProperty('background-color', '#ffffff', 'important');
+            alert("lef3");
+        }
 }
-function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
+function kattintasfrwd() {
+    counterVar -= 1;
+    if(counterVar < 1)
+        counterVar = 2;
+        alert(counterVar);
+                if(counterVar == 0){
+            document.body.style.backgroundColor = "none";
+            document.body.style.backgroundImage = "radial-gradient(#111, #323)";
+            document.body.style.backgroundAttachment = "fixed";
+            alert("lef");
+        }
+         else if(counterVar == 1){
+            document.body.style.backgroundImage = "none";
+            alert("lef2");
+        }
+        else{
+            document.body.style.backgroundColor = "none";
+            document.body.style.setProperty('background-color', '#ffffff', 'important');
+            alert("lef3");
+        }
 }

3e9ff158aa8f719bc98702a96472f0bd707c56f8
Kriszchan <panyik.krisztian@students.jedlik.eu>
2022-04-18 23:13:34 +0200

    enter

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 var counterVar = 0;
+
 function changeBackground() {
-    document.body.style.backgroundColor = "none";
-    document.body.style.backgroundImage = "radial-gradient(#111, #323)";
-    document.body.style.backgroundAttachment = "fixed";
+	document.body.style.backgroundColor = "none";
+	document.body.style.backgroundImage = "radial-gradient(#111, #323)";
+	document.body.style.backgroundAttachment = "fixed";
+    document.querySelector('.carousel-control-prev-icon').style.setProperty('filter', 'invert(100%)', 'important');
+    document.querySelector('.carousel-control-next-icon').style.setProperty('filter', 'invert(100%)', 'important');
 }
+
 function kattintasbkwd() {
-    counterVar += 1;
-    if(counterVar > 2)
-        counterVar = 0;
-        if(counterVar == 0){
-            document.body.style.backgroundColor = "none";
-            document.body.style.backgroundImage = "radial-gradient(#111, #323)";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-         else if(counterVar == 1){
-            document.body.style.backgroundImage = "none";
-        }
-        else{
-            document.body.style.backgroundColor = "none";
-            document.body.style.setProperty('background-color', '#ffffff', 'important');
-        }y
+	counterVar += 1;
+	if(counterVar > 2) counterVar = 0;
+	if(counterVar == 0) {
+		document.body.style.backgroundColor = "none";
+		document.body.style.backgroundImage = "radial-gradient(#111, #323)";
+		document.body.style.backgroundAttachment = "fixed";
+	} else if(counterVar == 1) {
+		document.body.style.backgroundImage = "none";
+        document.body.style.backgroundColor = "#2e2e2e";
+	} else if(counterVar == 2) {
+		document.body.style.backgroundColor = "none";
+		document.body.style.setProperty('background-color', '#ffffff', 'important');
+	}
+	y
 }
+
 function kattintasfrwd() {
-    counterVar -= 1;
-    if(counterVar < 1)
-        counterVar = 2;
-        alert(counterVar);
-                if(counterVar == 0){
-            document.body.style.backgroundColor = "none";
-            document.body.style.backgroundImage = "radial-gradient(#111, #323)";
-            document.body.style.backgroundAttachment = "fixed";
-        }
-         else if(counterVar == 1){
-            document.body.style.backgroundImage = "none";
-            document.body.style.backgroundColor = "#2e2e2e";
-        }
-        else if(counterVar == 2){
-            document.body.style.backgroundImage = "none";
-            document.body.style.backgroundColor = "none";
-            document.body.style.setProperty('background-color', '#f8eeec', 'important');
-            document.querySelector('.carousel-control-prev-icon').style.setProperty('filter', 'invert(100%)', 'important');
-            document.querySelector('.carousel-control-next-icon').style.setProperty('filter', 'invert(100%)', 'important');
-        }
+	counterVar -= 1;
+	if(counterVar < 0) counterVar = 2;
+	alert(counterVar);
+	if(counterVar == 0) {
+		document.body.style.backgroundColor = "none";
+		document.body.style.backgroundImage = "radial-gradient(#111, #323)";
+		document.body.style.backgroundAttachment = "fixed";
+	} else if(counterVar == 1) {
+		document.body.style.backgroundImage = "none";
+		document.body.style.backgroundColor = "#2e2e2e";
+	} else if(counterVar == 2) {
+		document.body.style.backgroundImage = "none";
+		document.body.style.backgroundColor = "none";
+		document.body.style.setProperty('background-color', '#f8eeec', 'important');
+	}
 }
